5d9277fa229cfc1184d6a81c084468d7c24f952f
akmaldika <13521070@mahasiswa.itb.ac.id>
2025-04-11 17:17:40 +0700

    feat: add material route in main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
-from app.routes.interacts import router as interacts_router
 
-app = FastAPI()
+from app.routes.interacts import router as interacts_router
+from app.routes.materials import router as materials_router
+
+from app.core.config import settings
+
+app = FastAPI(
+    title="AuraLearn API",
+    description="Backend API for AuraLearn Application",
+    version="0.1.0",
+)
 
 app.include_router(interacts_router, prefix="/api")
+app.include_router(materials_router, prefix="/api")
